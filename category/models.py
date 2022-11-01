f759b5dc8951fa7d77699dd2dabc85c992a0d6b2
Qiese <hoangtrungbi2002@gmail.com>
2022-11-01 21:53:47 +0700

    Add class about Category

---
+++ category/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=255, blank=True)
+    category_image = models.ImageField(upload_to='photo/categories/', blank=True)
+
+    class Mete:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category_name
