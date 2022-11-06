183c52dd3a5b3a0b5e80d0f80c9665728b382fa2
Qiese <hoangtrungbi2002@gmail.com>
2022-11-06 14:21:33 +0700

    Commit

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-#    path('', views.cart, name='cart'),
+   path('', views.cart, name='cart'),
    path('add_cart/<int:product_id>/',views.add_cart, name='add_cart'),
 #    path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
 #    path('remove_cart_itema/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Product
-
+from .models import Variation
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'category', 'created_date', 'is_available')
     prepopulated_fields = {'slug': ('product_name',)}
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active', 'created_date')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation,VariationAdmin)
