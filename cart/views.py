61f275063c163b8789e9544652fd97817d029a6f
Qiese <hoangtrungbi2002@gmail.com>
2022-11-07 17:48:53 +0700

    Commit

--- cart/views.py
+++ cart/views.py
@@ -19,7 +19,7 @@ def cart(request, total=0, quantity=0, cart_item=None):
         if request.user.is_authenticated:
             cart_items = CartItem.objects.filter(user=request.user, is_active=True)
         else:
-            cart = Cart.objects.get(cart_id=_cart_id, is_active=True)
+            cart = Cart.objects.get(cart_id=_cart_id(request=request))
             cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += cart_item.product.price * cart_item.quantity
