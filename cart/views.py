4f3b40c73823450927d8960150768947fd131468
Qiese <hoangtrungbi2002@gmail.com>
2022-11-07 15:51:03 +0700

    Created Cart Function

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import request
 from django.shortcuts import redirect
+from cart.models import Cart, CartItem
 from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 def _cart_id(request):
     cart_id = request.session.session_key
     if not cart_id:
@@ -11,3 +13,26 @@ def _cart_id(request):
 def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     return redirect('cart')
+
+def cart(request, total=0, quantity=0, cart_item=None):
+    try: 
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id, is_active=True)
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+        tax = total * 2/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax if "tax" in locals() else "",
+        'grand_total': grand_total
+    }
+    return render(request, 'store/cart.html', context=context)
