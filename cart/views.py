1010828c025503bc4201758593aa87285cf71a04
Qiese <hoangtrungbi2002@gmail.com>
2022-11-08 14:24:28 +0700

    Remove Cart Item function

--- cart/views.py
+++ cart/views.py
@@ -153,3 +153,24 @@ def remove_cart(request, product_id, cart_item_id):
     except Exception:
         pass
     return redirect('cart')
+
+def remove_cart_item(request, product_id, cart_item_id):
+    product = get_object_or_404(Product, id = product_id)
+    try: 
+        if request.user.is_authenticated:
+            cart_item = CartItem.objects.get(
+                id = cart_item_id,
+                product = product,
+                user = request.user
+            )
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request=request))
+            cart_item = CartItem.objects.get(
+                id = cart_item_id,
+                product = product,
+                cart = cart
+            )
+        cart_item.delete()
+    except Exception:
+        pass
+    return redirect('cart')
