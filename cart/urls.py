f9125783158b15b8d43513f8ce00a13453cfb27b
Qiese <hoangtrungbi2002@gmail.com>
2022-11-08 14:17:36 +0700

    Function Remove Cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
    path('', views.cart, name='cart'),
    path('add_cart/<int:product_id>/',views.add_cart, name='add_cart'),
-#    path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
+   path('remove_cart/<int:product_id>/<int:cart_item_id>/', views.remove_cart, name='remove_cart'),
 #    path('remove_cart_itema/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import request
 from django.shortcuts import redirect
 from cart.models import Cart, CartItem
@@ -10,6 +10,30 @@ def _cart_id(request):
     if not cart_id:
         cart_id = request.session.create()
     return cart_id
+
+def cart(request, total=0, quantity=0, cart_item=None):
+    try: 
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request=request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+        tax = total * 2/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax if "tax" in locals() else "",
+        'grand_total': grand_total
+    }
+    return render(request, 'store/cart.html', context=context)
+
 
 def add_cart(request, product_id):
     current_user = request.user
@@ -104,25 +128,28 @@ def add_cart(request, product_id):
                 cart_item.variations.add(item)
         cart_item.save()
         return redirect('cart')
-def cart(request, total=0, quantity=0, cart_item=None):
-    try: 
+
+def remove_cart(request, product_id, cart_item_id):
+    product = get_object_or_404(Product, id= product_id)
+    try:
         if request.user.is_authenticated:
-            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+            cart_item = CartItem.objects.get(
+                id = cart_item_id,
+                product = product,
+                user= request.user
+            )
         else:
-            cart = Cart.objects.get(cart_id=_cart_id(request=request))
-            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
-        for cart_item in cart_items:
-            total += cart_item.product.price * cart_item.quantity
-            quantity += cart_item.quantity
-        tax = total * 2/100
-        grand_total = total + tax
-    except ObjectDoesNotExist:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_item = CartItem.objects.get(
+                id = cart_item_id,
+                product = product,
+                cart = cart
+            )
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except Exception:
         pass
-    context = {
-        'total': total,
-        'quantity': quantity,
-        'cart_items': cart_items,
-        'tax': tax if "tax" in locals() else "",
-        'grand_total': grand_total
-    }
-    return render(request, 'store/cart.html', context=context)
+    return redirect('cart')
