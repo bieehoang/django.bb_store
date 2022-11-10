1eefeff83c1c25530227d2dcdd10668612ffaabd
Qiese <hoangtrungbi2002@gmail.com>
2022-11-11 02:34:10 +0700

    First commit from desktop

--- core/settings.py
+++ core/settings.py
@@ -24,8 +24,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS =env('ALLOWED_HOSTS') 
 
 # Application definition
 
