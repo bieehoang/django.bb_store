a96260671017fde2d3ca124f513dcf1ccb7f342b
Qiese <hoangtrungbi2002@gmail.com>
2022-11-10 20:41:59 +0700

    Gmail Connect

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from acounts.models import Account
+from store.models import Product, Variation
 
-# Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    payment_id = models.CharField(max_length = 100)
+    payment_method = models.CharField(max_length = 100)
+    amount_paid = models.CharField(max_length =100)
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.payment_id
+
+
