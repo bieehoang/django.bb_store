bf4b655df89341311398a71d98e61ed195dcb9af
Qiese <hoangtrungbi2002@gmail.com>
2022-11-19 15:52:11 +0700

    Configured AdminSite more Private

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from django.urls import include,path
 from . import views
 from django.conf.urls.static import static 
+import os
+import environ
+from pathlib import Path
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(env('ADMIN_SITE'), admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('carts/', include('cart.urls')),
