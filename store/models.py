0e4787af9155d724a9a05c6d2102bf24e986e490
Qiese <hoangtrungbi2002@gmail.com>
2022-11-29 17:19:02 +0700

    #12 - bieehoang-desktop

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=800, blank=True)
-    price = models.FloatField()
+    price = models.IntegerField()
     image = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
